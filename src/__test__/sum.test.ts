e42eadfa6b582ae4f6a1290d9eb0bfb232243d10
Jason <atwill007@163.com>
2022-04-01 23:50:28 +0800

    test: Supplementary test cases

--- src/__test__/sum.test.ts
+++ src/__test__/sum.test.ts
@@ -2,6 +2,7 @@ import { sum } from '../index'
 
 test('number utils sum test', () => {
   expect(sum()).toEqual(0)
+  expect(sum(0)).toEqual(0)
   expect(sum(1, 2, 3)).toEqual(6)
   expect(sum(1.1, 2.2)).toEqual(3.3000000000000003)
 })
