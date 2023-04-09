b059a537c846ccef71d0b4941ea634a666046b9c
atwill007 <sky6city@hotmail.com>
2023-04-09 21:32:02 +0800

    feat: Optimize toFixed logic

--- src/__test__/toFixed.test.ts
+++ src/__test__/toFixed.test.ts
@@ -1,5 +1,7 @@
 import { toFixed } from '../index'
-test('number utils toFixed test', () => {
-  expect(toFixed(123.123, 2)).toBe('123.12')
-  expect(toFixed(12345.12345, 4)).toBe('12345.1234')
-})
+
+  test('number utils toFixed test', () => {
+    expect(toFixed(123.123, 2)).toBe('123.12')
+    expect(toFixed(123.123, 4)).toBe('123.1230')
+    expect(toFixed(12345.12345, 4)).toBe('12345.1235')
+  })
--- src/toFixed.ts
+++ src/toFixed.ts
@@ -1,9 +1,18 @@
+import { precise } from './precise';
+
 /**
  * toFixed
  * @param {Number | String} source
- * @param {Number} length
+ * @param {Number} digits
  */
-export function toFixed(source: number | string, length: number = 2) {
-  const str: string = `${source}`
-  return parseFloat(str).toFixed(length)
+export function toFixed(source: number | string, digits: number = 2): string {
+  let num: number = parseFloat (`${source}`);
+
+  const times = Math.pow(10, digits)
+  // 0.5 为了四舍五入
+  num = precise(num * times + 0.5);
+  // 去除小数
+  num = parseInt(`${num}`, 10) / times
+
+  return precise(num).toFixed(digits)
 }
